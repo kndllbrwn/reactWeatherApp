803ed8dfb3939377f3a12509cc8936a1c7925932
kndllbrwn <kndllbrwn1914@gmail.com>
2017-02-18 15:37:38 -0500

    Added component properties

--- public/app.jsx
+++ public/app.jsx
@@ -1,15 +1,25 @@
 var Greeter = React.createClass({
+        getDefaultProps: function () {
+            return {
+                name: 'React',
+                message: 'Text from React.'
+            };
+        },
     render: function (){
+        var name = this.props.name;
+        var message = this.props.message;
         return(
             <div>
-                <h1>Hello React2</h1>
-                <p> Here is a paragraph from a React component!</p>
+                <h1>Hello {name}</h1>
+                <p>{message  + '!!'}</p>
             </div>
         );
     }
 }); 
 
+var firstName = 'Ken';
+var message = 'This is a paragraph from a React component!'
 ReactDOM.render(
-    <Greeter/>,
+    <Greeter name={firstName} message={message}/>,
     document.getElementById('app')
 );

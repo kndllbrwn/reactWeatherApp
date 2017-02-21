805171ed00e047ca093e794680b299e06516f4ff
kndllbrwn <kndllbrwn1914@gmail.com>
2017-02-20 20:52:56 -0500

    Added aliases to webpack config file

--- public/app.jsx
+++ public/app.jsx
@@ -1,10 +1,10 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var Greeter = require('./components/Greeter');
+var Greeter = require('Greeter');
 
 
 
-var firstName = 'Ken';
+var firstName = 'KenB';
 var message = 'This is a paragraph from a React component!'
 ReactDOM.render(
     <Greeter name={firstName} message={message}/>,

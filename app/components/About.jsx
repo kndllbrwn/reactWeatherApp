47688c604f170afd10d35d68447c1d2db7718103
kndllbrwn <kndllbrwn1914@gmail.com>
2017-02-22 22:00:07 -0500

    Added style to about page and links to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,27 @@ var React = require('react');
 var About = () => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the About page!</p>
+      <h1 className="text-center">About</h1>
+        <div className="callout primary">
+          <p>
+            This is a weather app I built to get practical experience with React!
+          </p>
+          <p>
+            Here are some of the tools I used:
+          </p>
+          <ul>
+            <li>
+              <a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework used.
+            </li>
+            <li>
+              <a href="http://openweathermap.org">Open Weather Map</a> - I used Open Weather Map to search for weather data by city name.
+            </li>
+            <li>
+              <a href="https://github.com/kndllbrwn/reactWeatherApp">My Github Repo</a> - This is a link to my repo!
+            </li>
+          </ul>
+
+        </div>
     </div>
   )
 };

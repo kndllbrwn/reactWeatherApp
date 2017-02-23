790fc48437c17f64f7f2544451cd04ec809a75af
kndllbrwn <kndllbrwn1914@gmail.com>
2017-02-22 19:14:43 -0500

    refactored modules to use arrow functions, v1 ready!

--- app/components/weatherComponents/WeatherMessage.jsx
+++ app/components/weatherComponents/WeatherMessage.jsx
@@ -1,13 +1,9 @@
 var React = require('react');
 
-var WeatherMessage = React.createClass({
-    render: function () {
-        var location = this.props.location;
-        var temp = this.props.temp;
+var WeatherMessage = ({location, temp}) => {
         return(
             <h3>It's {temp} in {location}.</h3>
         )
-    }
-});
+};
 
 module.exports = WeatherMessage;
